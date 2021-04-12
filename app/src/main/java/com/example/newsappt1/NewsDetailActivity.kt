63012f7da892c3c077798fe1d2dfd4867fa85fab
Bruno Mendonca <brunomendonca@tokenlab.com.br>
2021-04-12 08:27:05 -0300

    Ajustes para NewsDetailActivity.kt ser classe burra

--- app/src/main/java/com/example/newsappt1/NewsDetailActivity.kt
+++ app/src/main/java/com/example/newsappt1/NewsDetailActivity.kt
@@ -1,10 +1,8 @@
 package com.example.newsappt1
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.webkit.URLUtil
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -26,48 +24,55 @@ class NewsDetailActivity : AppCompatActivity() {
         binding = ActivityNewsDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val news = intent.getParcelableExtra<News>(NEWS_DETAIL_KEY)
-        val viewModelFactory = NewsDetailViewModelFactory(news!!)
+        val receivedNews = intent.getParcelableExtra<News>(NEWS_DETAIL_KEY)
+        val viewModelFactory = NewsDetailViewModelFactory(receivedNews!!)
         viewModel = ViewModelProvider(this, viewModelFactory).get(NewsDetailViewModel::class.java)
 
-        binding.newsTitle.text = viewModel.news.title
+        viewModel.newsDetail.observe(this) { news ->
 
-        if (viewModel.news.description != null) binding.newsDescription.text =
-            viewModel.news.description
-        else binding.newsDescription.visibility = View.GONE
+            binding.newsTitle.text = news.title
 
-        if (viewModel.news.content != null) binding.newsContent.text = viewModel.news.content
-        else binding.newsContent.visibility = View.GONE
+            if (news.description != null) binding.newsDescription.text =
+                news.description
+            else binding.newsDescription.visibility = View.GONE
 
-        binding.newsSource.text = getString(
-            R.string.news_source,
-            viewModel.news.author ?: getString(R.string.unknown),
-            viewModel.news.source.name
-        )
+            if (news.content != null) binding.newsContent.text = news.content
+            else binding.newsContent.visibility = View.GONE
 
-        binding.newsLastUpdate.text =
-            getString(R.string.news_lastupdate, viewModel.news.lastUpdate)
+            binding.newsSource.text = getString(
+                R.string.news_source,
+                news.author ?: getString(R.string.unknown),
+                news.source.name
+            )
 
-        Glide
-            .with(this)
-            .load(viewModel.news.imageUrl)
-            .placeholder(R.drawable.ic_no_image)
-            .into(binding.newsImage)
+            binding.newsLastUpdate.text =
+                getString(R.string.news_lastupdate, news.lastUpdate)
 
-        binding.btnShowNews.setOnClickListener {
-            if (URLUtil.isValidUrl(viewModel.news.newsUrl)) {
-                val webpage: Uri = Uri.parse(viewModel.news.newsUrl)
-                val intent = Intent(Intent.ACTION_VIEW, webpage)
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivity(intent)
-                } else {
-                    Toast.makeText(this, "You need to install a browser", Toast.LENGTH_SHORT)
-                        .show()
-                }
+            Glide
+                .with(this)
+                .load(news.imageUrl)
+                .placeholder(R.drawable.ic_no_image)
+                .into(binding.newsImage)
+
+            binding.btnShowNews.setOnClickListener {
+                viewModel.onShowNewsClicked(news)
+            }
+
+        }
+
+        viewModel.navigationShowNews.observe(this) { uri ->
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
             } else {
-                Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show()
+                viewModel.onShowNewsResolveActivityFail()
             }
         }
+
+        viewModel.message.observe(this) { message ->
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 }
--- app/src/main/java/com/example/newsappt1/NewsDetailViewModel.kt
+++ app/src/main/java/com/example/newsappt1/NewsDetailViewModel.kt
@@ -1,5 +1,39 @@
 package com.example.newsappt1
 
+import android.net.Uri
+import android.webkit.URLUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NewsDetailViewModel(val news: News): ViewModel()
+class NewsDetailViewModel(news: News) : ViewModel() {
+
+    private val _newsDetail: MutableLiveData<News> = MutableLiveData()
+    val newsDetail: LiveData<News>
+        get() = _newsDetail
+
+    private val _navigationShowNews: MutableLiveData<Uri> = MutableLiveData()
+    val navigationShowNews: LiveData<Uri>
+        get() = _navigationShowNews
+
+    private val _message: MutableLiveData<String> = MutableLiveData()
+    val message: LiveData<String>
+        get() = _message
+
+    init {
+        _newsDetail.value = news
+    }
+
+    fun onShowNewsClicked(news: News) {
+        if (URLUtil.isValidUrl(news.newsUrl)) {
+            val webpage: Uri = Uri.parse(news.newsUrl)
+            _navigationShowNews.value = webpage
+        } else {
+            _message.value = "Invalid URL"
+        }
+    }
+
+    fun onShowNewsResolveActivityFail() {
+        _message.value = "You need to install a browser"
+    }
+}
