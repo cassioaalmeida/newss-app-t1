b7ef0cc4149f6c7080a4439321ac814609d2172c
Bruno Mendonca <brunomendonca@tokenlab.com.br>
2021-04-16 09:17:09 -0300

    Salvando NewsList em cache

--- app/src/main/java/com/example/newsappt1/presentation/scene/newslist/NewsListViewModel.kt
+++ app/src/main/java/com/example/newsappt1/presentation/scene/newslist/NewsListViewModel.kt
@@ -8,11 +8,16 @@ import com.example.newsappt1.data.model.News
 import com.example.newsappt1.data.model.NewsList
 import com.example.newsappt1.presentation.common.ScreenState
 import com.example.newsappt1.data.remote.RetrofitInitializer
+import io.paperdb.Paper
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class NewsListViewModel() : ViewModel() {
+
+    companion object {
+        private const val NEWS_LIST_KEY = "NEWS_LIST_KEY"
+    }
 
     private val _screenState: MutableLiveData<ScreenState<List<News>>> = MutableLiveData()
     val screenState: LiveData<ScreenState<List<News>>>
@@ -49,19 +54,36 @@ class NewsListViewModel() : ViewModel() {
 
         service.getTopHeadlines("br").enqueue(object : Callback<NewsList> {
             override fun onResponse(call: Call<NewsList>, response: Response<NewsList>) {
-                // verifica se o retorno foi feito com sucesso
                 if (response.isSuccessful && response.body() != null && response.body()!!.items.isNotEmpty()) {
-                    // tenho acesso a minha lista de notícias
-                    _screenState.value =
-                        ScreenState.Success(response.body()!!.items as ArrayList<News>)
+                    val remoteNews: List<News> = response.body()!!.items
+                    saveInCache(remoteNews)
+                    _screenState.value = ScreenState.Success(remoteNews)
                 } else {
-                    _screenState.value = ScreenState.Error()
+                    val cacheNews: List<News>? = getDataFromCache()
+                    if(cacheNews != null) {
+                        _screenState.value = ScreenState.Success(cacheNews)
+                    } else {
+                        _screenState.value = ScreenState.Error()
+                    }
                 }
             }
 
             override fun onFailure(call: Call<NewsList>, t: Throwable) {
-                _screenState.value = ScreenState.Error()
+                val cacheNews: List<News>? = getDataFromCache()
+                if(cacheNews != null) {
+                    _screenState.value = ScreenState.Success(cacheNews)
+                } else {
+                    _screenState.value = ScreenState.Error()
+                }
             }
         })
+    }
+
+    private fun saveInCache(newsList: List<News>) {
+        Paper.book().write(NEWS_LIST_KEY, newsList)
+    }
+
+    private fun getDataFromCache(): List<News>? {
+        return Paper.book().read(NEWS_LIST_KEY)
     }
 }
